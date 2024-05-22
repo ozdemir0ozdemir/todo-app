3827cc011037ada4fd0e4817448b50395e7fe61e
Özdemir Özdemir <ozdemirozdemir@hotmail.com.tr>
2024-05-22 19:23:56 +0300

    Initial Commit

---
+++ todo-app-backend/src/test/java/ozdemir0zdemir/todoappbackend/TodoAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ozdemir0zdemir.todoappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
